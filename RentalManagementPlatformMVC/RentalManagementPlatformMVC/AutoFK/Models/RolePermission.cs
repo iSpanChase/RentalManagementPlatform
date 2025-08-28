dacd4243f1e7b9e1bb9d7aea2325c39d191bad54
Chase <mmm998520@gmail.com>
2025-08-28 17:19:18 +0800

    自動對應FK功能製作

---
+++ RentalManagementPlatformMVC/RentalManagementPlatformMVC/AutoFK/Models/RolePermission.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AutoFK.Models;
+
+public partial class RolePermission
+{
+    public int RolePermissionId { get; set; }
+
+    public int RoleId { get; set; }
+
+    public int PermissionId { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+}
---
+++ RentalManagementPlatformMVC/RentalManagementPlatformMVC/AutoFK/Models/RoomList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AutoFK.Models;
+
+public partial class RoomList
+{
+    public int RoomId { get; set; }
+
+    public int? AddressId { get; set; }
+
+    public int? HostId { get; set; }
+
+    public string? Title { get; set; }
+
+    public string? Description { get; set; }
+
+    public int? MaxGuests { get; set; }
+
+    public decimal? PricePerNight { get; set; }
+
+    public string? Status { get; set; }
+
+    public DateTime? CreatedAt { get; set; }
+
+    public DateTime? UpdatedAt { get; set; }
+}
