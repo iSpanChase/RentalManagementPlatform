66a2c0967d3c49941b9a5cabac9ae83b45e7ed90
Jamie Lee <mmorpg176@gmail.com>
2025-09-06 12:45:37 +0800

    Merge from dev and solve conflict

--- RentalManagementPlatformMVC/RentalManagementPlatformMVC/RentalManagementPlatformMVC/Program.cs
+++ RentalManagementPlatformMVC/RentalManagementPlatformMVC/RentalManagementPlatformMVC/Program.cs
@@ -1,9 +1,6 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using RentalManagementPlatformMVC.Data;
-using RentalManagementPlatformMVC.Models;
-using RentalManagementPlatformMVC.Repositories;
-using RentalManagementPlatformMVC.Services;
 
 namespace RentalManagementPlatformMVC
 {
