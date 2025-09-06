5b5eb14b4e47f717324fdb2f4a2d1cf824e5981c
Jamie Lee <mmorpg176@gmail.com>
2025-09-06 12:15:05 +0800

    修正Merge衝突

---
+++ RentalManagementPlatformMVC/RentalManagementPlatformMVC/RentalManagementPlatformMVC/Models/Partials/Booking.Partical.cs
@@ -0,0 +1,10 @@
+﻿namespace RentalManagementPlatformMVC.Models
+{
+	public partial class Booking
+	{
+		public ICollection<BookingGuest> BookingGuests { get; set; } = new List<BookingGuest>();
+		public User Guest { get; set; }
+		public RoomList Room { get; set; }
+		public Coupon? Coupon { get; set; } = null;
+	}
+}
