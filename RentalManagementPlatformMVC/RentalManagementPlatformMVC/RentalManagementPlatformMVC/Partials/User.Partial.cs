70d41b00600ba7ea6fb601214452127c2218e27b
Jamie Lee <mmorpg176@gmail.com>
2025-09-09 16:27:55 +0800

    修改Merge衝突

--- RentalManagementPlatformMVC/RentalManagementPlatformMVC/RentalManagementPlatformMVC/Partials/User.Partial.cs
+++ RentalManagementPlatformMVC/RentalManagementPlatformMVC/RentalManagementPlatformMVC/Partials/User.Partial.cs
@@ -1,6 +1,5 @@
 ﻿namespace RentalManagementPlatformMVC.Models
 {
-	public partial class User
 	{
 		public ICollection<Booking> Bookings { get; set; } = new List<Booking>();
 		public ICollection<RoomList> RoomLists { get; set; } = new List<RoomList>();
