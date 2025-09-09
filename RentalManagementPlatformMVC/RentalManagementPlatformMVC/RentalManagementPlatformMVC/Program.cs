327eb31338aa0efcac6dad71c87b1d9df4fdb99e
Avery-TSE <every2001050174@gmail.com>
2025-09-09 16:24:18 +0800

    新增了FAQ的Area，將DBContext串接完成，實作了CRUD版面。更改了program.cs的Area渲染

--- RentalManagementPlatformMVC/RentalManagementPlatformMVC/RentalManagementPlatformMVC/Program.cs
+++ RentalManagementPlatformMVC/RentalManagementPlatformMVC/RentalManagementPlatformMVC/Program.cs
@@ -49,6 +49,10 @@ namespace RentalManagementPlatformMVC
             app.UseAuthorization();
 
             app.MapControllerRoute(
+                name: "areas",
+                pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+            app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
             app.MapRazorPages();
