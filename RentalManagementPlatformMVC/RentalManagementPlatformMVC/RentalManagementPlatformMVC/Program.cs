1fd9cbc4dba9e1f444c9a62895e78e39831d843b
Chase <mmm998520@gmail.com>
2025-09-10 12:49:35 +0800

    Revert "Merge branch 'dev' of https://github.com/iSpanChase/RentalManagementPlatform into dev"
    This reverts commit c13eeffbc9eb452d1e8bcc16ba8c0a2f34eddf74, reversing
    changes made to 2c954702df62be5b53d7b0d2688dcae7c44f8eaa.

--- RentalManagementPlatformMVC/RentalManagementPlatformMVC/RentalManagementPlatformMVC/Program.cs
+++ RentalManagementPlatformMVC/RentalManagementPlatformMVC/RentalManagementPlatformMVC/Program.cs
@@ -1,10 +1,6 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using RentalManagementPlatformMVC.Areas.UserManagement.UserRepositories;
-using RentalManagementPlatformMVC.Areas.Management.Repository;
-using RentalManagementPlatformMVC.Areas.Management.Repository.Interfaces;
-using RentalManagementPlatformMVC.Areas.Management.Services;
-using RentalManagementPlatformMVC.Areas.Management.Services.Interfaces;
 using RentalManagementPlatformMVC.Data;
 using RentalManagementPlatformMVC.Models;
 using RentalManagementPlatformMVC.Repositories;
@@ -18,26 +14,11 @@ namespace RentalManagementPlatformMVC
         {
             var builder = WebApplication.CreateBuilder(args);
 
-
 			// Identity 用的 Context（連線字串用 DefaultConnection）
-			builder.Services.AddScoped<ICouponQueryService, CouponQueryService>();
-
 			builder.Services.AddDbContext<ApplicationDbContext>(options =>
 				options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-
 			// 業務資料表用的 Context（連線字串同樣指向同一顆 DB）
-			//service註冊
-			builder.Services.AddScoped<ICouponQueryService, CouponQueryService>();
-			builder.Services.AddScoped<CouponCommandService>();
-
-			//repository註冊
-			builder.Services.AddScoped<ICouponReadRepository, CouponReadRepository>();
-			builder.Services.AddScoped<ICouponWriteRepository, CouponWriteRepository>();
-
-			builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-
 			builder.Services.AddDbContext<RentalManagementPlatformSqlContext>(options =>
 				options.UseSqlServer(builder.Configuration.GetConnectionString("RentalManagementPlatformSql")));
 
@@ -53,12 +34,7 @@ namespace RentalManagementPlatformMVC
 			builder.Services.AddScoped<IPaymentRepository, PaymentRepository>();
 			builder.Services.AddScoped<IPaymentService, PaymentService>();
 
-
 			builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-			builder.Services.AddControllersWithViews();
-			//var app = builder.Build();
-
 
             builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
