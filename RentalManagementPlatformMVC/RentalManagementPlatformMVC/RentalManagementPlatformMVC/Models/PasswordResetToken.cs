0ca847724f24fbc581fc5185edf032b714cb3f4a
ZHAO199712 <ymsh20220@gmail.com>
2025-09-16 14:42:40 +0800

    FILEURL

---
+++ RentalManagementPlatformMVC/RentalManagementPlatformMVC/RentalManagementPlatformMVC/Models/PasswordResetToken.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RentalManagementPlatformMVC.Models;
+
+public partial class PasswordResetToken
+{
+    public int TokenId { get; set; }
+
+    public int UserId { get; set; }
+
+    public string TokenHash { get; set; } = null!;
+
+    public DateTime ExpiresAt { get; set; }
+
+    public DateTime? UsedAt { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public virtual User User { get; set; } = null!;
+}
