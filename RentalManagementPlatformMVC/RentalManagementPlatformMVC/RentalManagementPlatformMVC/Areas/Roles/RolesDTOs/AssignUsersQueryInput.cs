079dd3d6c2426d19d486be5844c99620c49f8082
TingYi <ian210235@gmail.com>
2025-09-18 11:21:44 +0800

    修改指派角色及指派權限的畫面和規則

---
+++ RentalManagementPlatformMVC/RentalManagementPlatformMVC/RentalManagementPlatformMVC/Areas/Roles/RolesDTOs/AssignUsersQueryInput.cs
@@ -0,0 +1,15 @@
+﻿namespace RentalManagementPlatformMVC.Areas.Roles.RolesDTOs
+{
+	public class AssignUsersQueryInput
+	{
+		public int RoleId { get; set; }
+
+		// 搜尋 / 排序 / 分頁
+		public string? Keyword { get; set; }
+		public string SortBy { get; set; } = "Name"; // 可用: Name, Username, Email
+		public bool Desc { get; set; } = false;
+
+		public int Page { get; set; } = 1;
+		public int PageSize { get; set; } = 10;
+	}
+}
