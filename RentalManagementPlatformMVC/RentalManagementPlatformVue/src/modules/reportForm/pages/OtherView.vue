c6b6555a4f944bea114b100463da5e3a639c666b
Chase <mmm998520@gmail.com>
2025-10-07 12:31:03 +0800

    Vue專案router、module架構製作

---
+++ RentalManagementPlatformMVC/RentalManagementPlatformVue/src/modules/reportForm/pages/OtherView.vue
@@ -0,0 +1,13 @@
+<!-- src/modules/reportForm/pages/OtherView.vue -->
+<template>
+<div>
+<h1>ReportForm Other</h1>
+<router-link :to="{ name: 'ReportForm.IndexView' }">Index</router-link>
+<router-view />
+</div>
+</template>
+
+
+<script setup>
+// setup script
+</script>
