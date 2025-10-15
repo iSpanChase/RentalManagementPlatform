311e080db6bf3ac5bc10740045a21e1d4de7e989
Chase <mmm998520@gmail.com>
2025-10-15 11:07:01 +0800

    前台網頁專案建置

---
+++ RentalManagementPlatformMVC/RentalManagementPlatformVue/src/router/modules/exampleA/pages/IndexView.vue
@@ -0,0 +1,13 @@
+<!-- src/modules/exampleA/pages/IndexView.vue -->
+<template>
+<div>
+<h1>Example A Index</h1>
+<router-link :to="{ name: 'ExampleA.OtherView' }">Other</router-link>
+<router-view />
+</div>
+</template>
+
+
+<script setup>
+// setup script
+</script>
