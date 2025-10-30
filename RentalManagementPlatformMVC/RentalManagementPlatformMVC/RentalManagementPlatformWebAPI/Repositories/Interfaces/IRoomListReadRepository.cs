b2d6caca847c08090703d644772243236af41c9f
zhao199712 <ymsh20220@gmail.com>
2025-10-30 13:15:21 +0800

    房源讀寫服務修改

--- RentalManagementPlatformMVC/RentalManagementPlatformMVC/RentalManagementPlatformWebAPI/Repositories/Interfaces/IRoomListReadRepository.cs
+++ RentalManagementPlatformMVC/RentalManagementPlatformMVC/RentalManagementPlatformWebAPI/Repositories/Interfaces/IRoomListReadRepository.cs
@@ -1,5 +1,8 @@
-using RentalManagementPlatformWebAPI.Models; // Changed to API's Models namespace
+using RentalManagementPlatformWebAPI.Models; 
 using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using RentalManagementPlatformWebAPI.DTOs; // Correct DTO namespace
 
 namespace RentalManagementPlatformWebAPI.Repositories.Interfaces
 {
@@ -10,5 +13,7 @@ namespace RentalManagementPlatformWebAPI.Repositories.Interfaces
         IQueryable<Address> GetAddresses();
         IQueryable<District> GetDistricts();
         IQueryable<City> GetCities();
+        Task<IEnumerable<(RoomList Room, User Host, decimal? RatingAvg, int ReviewsCount)>> GetRandomRoomsWithHostAsync(int count);
+        Task<(double? RatingAvg, int ReviewsCount)> GetRoomRatingStatsAsync(int roomId);
     }
 }
