afc8e3cb6877a0da667e850ceabba532d7f37b4c
zhao199712 <ymsh20220@gmail.com>
2025-10-30 15:38:46 +0800

    修正BUG

--- RentalManagementPlatformMVC/RentalManagementPlatformVue/src/main.ts
+++ RentalManagementPlatformMVC/RentalManagementPlatformVue/src/main.ts
@@ -4,16 +4,22 @@ import App from './App.vue'
 import router from './router'
 import { config, library, dom } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faChevronUp, faChevronDown, faCreditCard, faLock, faCheck, faUser, faLocationDot, faComment, faShieldHalved, faCircleInfo, faAlarmClock, faHouse, faAddressBook} from '@fortawesome/free-solid-svg-icons'
-import { faChevronUp, faChevronDown, faStar } from '@fortawesome/free-solid-svg-icons'
+import { 
+    faChevronUp, faChevronDown, faCreditCard, faLock, faCheck, faUser, 
+    faLocationDot, faComment, faShieldHalved, faCircleInfo, faAlarmClock, 
+    faHouse, faAddressBook, faStar 
+} from '@fortawesome/free-solid-svg-icons'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 
 import { VueQueryPlugin, QueryClient } from '@tanstack/vue-query';
 
 // 讓 Font Awesome 不自動插入 <style>，避免與 Vite 衝突
 config.autoAddCss = false
-library.add(faChevronUp, faChevronDown, faStar)
-library.add(faChevronUp, faChevronDown, faCreditCard, faLock, faCheck, faUser, faLocationDot, faComment, faShieldHalved, faCircleInfo, faAlarmClock, faHouse, faAddressBook);
+library.add(
+    faChevronUp, faChevronDown, faCreditCard, faLock, faCheck, faUser, 
+    faLocationDot, faComment, faShieldHalved, faCircleInfo, faAlarmClock, 
+    faHouse, faAddressBook, faStar
+);
 dom.watch()
 
 const app = createApp(App)
@@ -23,7 +29,6 @@ const queryClient = new QueryClient();
 app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.use(pinia);
-app.use(router)
 app.use(router);
 app.use(VueQueryPlugin, { queryClient });
 
